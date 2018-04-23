4fb88507221b0bda3a3cfa890b07cf86f72ad93a
Wayne Radinsky <waynerad@gmail.com>
2018-04-23 16:01:05 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # bfactor
 Factoring numbers with Bill
+
+So this repository started when one day my friend Bill asked me what the smallest number that had 36 factors was. He came up with an algorithm, which was to find the prime factorization of 36 (or whatever), then using those as the exponents (plus one) of the numbers used to create the number with 36 (or whatever) factors, distributing them in "reverse" order so the largest exponents are used on the smallest prime (2 has the most, then 3, etc).
+
+This instantly came up with 1,260 as the answer, which was correct. But does this algorithm *always* use the correct algorithm? To find out, I coded up code to test all the possibilities. It was a tricky combinatorics problem, but I figured out a way to do it. The way I do it is by using bitflags, one for each of the factors of 36 (or whatever), and, in the outer loop, it counts down from the number with all the bits set down to 0, and that represents the factors that will go in the first group, and in the first inner loop, it uses the outer loop as a mask to find all combinations of the bits not used in the outer loop, and that represents the factors that go in the second group, and in the next inner loop, it combines both of the outer loops to make a mask so it finds all combinations of bits not used by either of the outer two, and that represents the factors that go in the third group, and so on -- with no limit to the number of inner loops. In this way, the system can find all combinations of factors with no fixed number of groups.
+
+As it turns out, this method found repetitions and also combinations where not all the factors were used at all. So, added some code to combine all the bitflags from all the loops to verify that all the factors were being used. Didn't do anything about the repetitions, which comes from the fact that factors are repeated and the combinatorics code assumes they are all unique (e.g. 2, 2, 3, 3 for 36, the 2s and 3s are not unique, so we repeat them in the calculations). The code probably could be optimized to remove these duplicate calculations, which are harmless but waste a bit of computing power.
+
+Anyway, running Bill's algorithm against my "brute force" algorithm to test all combinations, we found Bills algorithm was almost always right -- but not always. The first number for which it was wrong was 8 factors -- I've been saying "or whatever" because we made the 36 a parameter that could be changed, and then looped through to find all the numbers that were the minimum for N factors where N goes in a sequence. For 8 factors, Bill's algorithm said 30, but mine said 24, and, 24 does indeed have 8 factors (1, 2, 3, 4, 6, 8, 12, and 24), so 24 is lower and is in fact the correct answer. (We ultimately verified this by Google search and the encyclopedia of integer sequences).
+
+The code here provides the following functions:
+
+findMinWithFactors() finds the smallest number with N factors, e.g. findMinWithFactors(36) finds the smallest number with 36 factors (which is 24), using my combinatorics algorithm.
+billAlgorithm() finds the smallest using Bill's algorithm.
+findBillAlgoDifferent() finds where the two algorithms are different.
+findSeries() generates a series of the smallest number with N factors for sequential N, and
+findHighlyCompositeNumbers(), finds a list of highly composite numbers, which I haven't mentioned thus far, but is what got me and Bill talking about "36 factors" in the first place
+
+In addition to all this, this code has some handy helper functions that you can use anywhere:
+
+calcPrimes(), which calculates primes (using the sieve of Eratosthenes algorithm), up to some limit (the other functions require a list of primes as input)
+calcFactors(), which finds the prime factors of some number
+countFactors(), which counts the number of factors a number has given its prime factorization
+showFactorization(), which displays the prime factorization to the user
+
+The code does have some issues, notably that, even when using unsigned 64-bit integers, it frequently overflows. To understand why this is the case, consider the prime number 67. The only numbers that have 67 factors are prime numbers to the 68th power. The smallest of these is 2^68. However, a 64-bit unsigned int can only go up to 2^65 - 1. Therefore it is easy to see, that as soon as you ask for the minimum number with N factors, as soon as N gets up to 67 this program will fail to come up with the answers. This can be remedied by using Go's math/big package for infinite-precision arithmetic. Bill wrote his program in Python, though, which automatically bumped his variables up to infinite precision ints, so was able to calculate all of these without modifying his code. However, since we are searching for the minimums, and as long as we're interested in minimums that aren't huge numbers (N highly composite instead of prime), this code will work ok even with this issue. But it's something to be aware of before you put this code into some production system or something.
+
---
+++ bfactors.go
@@ -0,0 +1,330 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// crude brute force integer exponentiation so we don't have to convert to float and back to use math.Exp()
+// we're using small enough ints that this should be ok
+func intexp(aa int, bb int) uint64 {
+	var abig uint64
+	var rr uint64
+	var prev uint64
+	rr = 1
+	prev = 1
+	abig = uint64(aa)
+	for ii := 0; ii < bb; ii++ {
+		prev = rr
+		rr *= abig
+		if rr < prev {
+			// This should be an error-prone way of detecting overflows
+			return 0 // too big for us to care about
+		}
+	}
+	return rr
+}
+
+// Sieve of Erastrathenes algorithm -- fastest way to get primes, but
+// you have to specify what number to go up to in advance
+func calcPrimes(upto int) []int {
+	sieve := make([]bool, upto)
+	result := make([]int, 0)
+
+	p := 2
+	z := p
+	for p < upto {
+		result = append(result, p)
+		z = p
+		for z < upto {
+			sieve[z] = true
+			z += p
+		}
+		p++
+		if p == upto {
+			return result
+		}
+		for sieve[p] {
+			p++
+			if p == upto {
+				return result
+			}
+		}
+	}
+	return result // we will never reach this line
+}
+
+// One fuction for factoring a number that gives back both an ordered
+// list of primes and exponents (factors and order) and an expanded
+// list without exponents (sometimes more useful)
+func calcFactors(primes []int, number int) (map[int]int, []int, []int) {
+	factors := make(map[int]int)
+	order := make([]int, 0)
+	expanded := make([]int, 0)
+	idx := 0
+	for number != 1 {
+		if (number % primes[idx]) == 0 {
+			_, ok := factors[primes[idx]]
+			if ok {
+				factors[primes[idx]]++
+			} else {
+				factors[primes[idx]] = 1
+				order = append(order, primes[idx])
+			}
+			expanded = append(expanded, primes[idx])
+			number = number / primes[idx]
+		} else {
+			idx++
+		}
+	}
+	return factors, order, expanded
+}
+
+// finds the smallest using Bill's algorithm.
+func billAlgorithm(factors []int, primes []int) uint64 {
+	var total uint64
+	total = 1
+	currentPrimeIdx := 0
+	for idx := len(factors) - 1; idx >= 0; idx-- {
+		total *= intexp(primes[currentPrimeIdx], factors[idx]-1)
+		currentPrimeIdx++
+	}
+	return total
+}
+
+func showIdxes(idxes []int, currentIdx int, comboResult int) {
+	for ii := 0; ii <= currentIdx; ii++ {
+		fmt.Print("    ", idxes[ii])
+	}
+	fmt.Println(" -> ", comboResult)
+}
+
+func usable(idxes []int, currentIdx int, bitflags int) bool {
+	acc := 0
+	for jj := 0; jj <= currentIdx; jj++ {
+		acc |= idxes[jj]
+	}
+	if acc == bitflags {
+		return true
+	}
+	return false
+}
+
+func doCrazyCalculation(idxes []int, currentIdx int, factors []int, primes []int) uint64 {
+	// uncomment the prints in this function to see the expansion of all the exponents multiplied together to get a number
+	var total uint64
+	total = 1
+	primenum := 0
+	for ii := 0; ii <= currentIdx; ii++ {
+		thisNum := 1
+		flags := idxes[ii]
+		bit := 1
+		number := 0
+		for flags != 0 {
+			if (flags & bit) != 0 {
+				thisNum *= factors[number]
+				flags -= bit
+			}
+			bit *= 2
+			number++
+		}
+		total *= intexp(primes[primenum], thisNum-1)
+		// fmt.Print(" * ",primes[primenum], "^", thisNum - 1)
+		primenum++
+	}
+	// fmt.Println(" = ", total)
+	return total
+}
+
+func findAlls(factors []int, primes []int) uint64 {
+	var bitflags int
+	bitflags = int(intexp(2, len(factors))) - 1
+	idxes := make([]int, len(factors)+1)
+	masks := make([]int, len(factors)+1)
+	idxes[0] = bitflags + 1
+	masks[0] = bitflags
+	currentIdx := 0
+	keepgoing := true
+	iterz := 65536 // Sanity loop breaker if things run forever
+	var minimum uint64
+	minimum = 0 // flag to indicate not set yet
+	for keepgoing {
+		idxes[currentIdx]--
+		for (idxes[currentIdx] & masks[currentIdx]) != idxes[currentIdx] {
+			idxes[currentIdx]--
+		}
+		if idxes[currentIdx] == 0 {
+			// we hit the end -- back up to previous level
+			currentIdx--
+			if usable(idxes, currentIdx, bitflags) {
+				comboResult := doCrazyCalculation(idxes, currentIdx, factors, primes)
+				// showIdxes(idxes, currentIdx, comboResult)
+				if minimum == 0 {
+					minimum = comboResult
+				} else {
+					if comboResult < minimum {
+						if comboResult > 0 {
+							minimum = comboResult
+						}
+					}
+				}
+			}
+			// no previous level? exit the whole function!
+			if currentIdx < 0 {
+				return minimum
+			}
+		} else {
+			// else -- advance to NEXT level
+			currentIdx++
+			idxes[currentIdx] = bitflags + 1
+			// sanity check
+			// if (masks[currentIdx-1] - idxes[currentIdx-1]) != (masks[currentIdx-1] & (bitflags - idxes[currentIdx-1])) {
+			//	fmt.Println("idxes", idxes)
+			//	fmt.Println("masks", masks)
+			//	fmt.Println("currentIdx", currentIdx)
+			//	panic("Mask subtract failure")
+			// }
+			masks[currentIdx] = masks[currentIdx-1] - idxes[currentIdx-1]
+		}
+
+		// this is to break the loop if it becomes endless -- which should never happen
+		iterz--
+		if iterz == 0 {
+			keepgoing = false
+		}
+	}
+	return minimum
+}
+
+// finds the smallest number with N factors, e.g. findMinWithFactors(36)
+// finds the smallest number with 36 factors (which is 24), using my
+// combinatorics algorithm.
+func findMinWithFactors(numFactors int) {
+	primes := calcPrimes(100)
+	factors, order, expanded := calcFactors(primes, numFactors)
+	fmt.Println("factors", factors)
+	fmt.Println("order", order)
+	fmt.Println("expanded", expanded)
+	billMin := billAlgorithm(expanded, primes)
+	fmt.Println("billMin", billMin)
+	minimum := findAlls(expanded, primes)
+	fmt.Println("minimum", minimum)
+}
+
+// finds where the two algorithms are different.
+func findBillAlgoDifferent() {
+	primes := calcPrimes(1000)
+	for numFactors := 1; numFactors < 1000; numFactors++ {
+		_, _, expanded := calcFactors(primes, numFactors)
+		billMin := billAlgorithm(expanded, primes)
+		minimum := findAlls(expanded, primes)
+		if billMin != minimum {
+			if minimum > 0 {
+				// fmt.Println("Bill min is different", numFactors)
+				// fmt.Println("    Bill min:", billMin)
+				// fmt.Println("    minimum:", minimum)
+				fmt.Println("(", minimum, ",", billMin, ")")
+			}
+		}
+	}
+}
+
+// generates a series of the smallest number with N factors for sequential N
+func findSeries(upto int) {
+	primes := calcPrimes(upto)
+	for numFactors := 1; numFactors < upto; numFactors++ {
+		_, _, expanded := calcFactors(primes, numFactors)
+		minimum := findAlls(expanded, primes)
+		fmt.Println(numFactors, minimum)
+		// fmt.Println(minimum)
+	}
+}
+
+// counts the number of factors a number has given its prime factorization
+func countFactors(primes []int, factors map[int]int, order []int) int {
+	total := 1
+	for ii := 0; ii < len(order); ii++ {
+		total *= factors[order[ii]] + 1
+	}
+	return total
+}
+
+func intToStr(nn int) string {
+	return strconv.FormatInt(int64(nn), 10)
+}
+
+// displays the prime factorization to the user
+func showFactorization(primes []int, factors map[int]int, order []int) {
+	if len(order) == 0 {
+		return
+	}
+	result := ""
+	for ii := 0; ii < len(order); ii++ {
+		result += " * " + intToStr(order[ii]) + "^" + intToStr(factors[order[ii]])
+	}
+	result = result[3:]
+	fmt.Println(result)
+}
+
+// finds a list of highly composite numbers, which is what got me and
+// Bill talking about "36 factors" in the first place
+func findHighlyCompositeNumbers(upto int) {
+	primes := calcPrimes(upto)
+	maxSoFar := 0
+	hasBeenUsed := make(map[int]bool)
+	for number := 1; number < upto; number++ {
+		factors, order, _ := calcFactors(primes, number)
+		numFactors := countFactors(primes, factors, order)
+		if numFactors >= maxSoFar {
+			foundNew := false
+			firstAppearance := ""
+			for ii := 0; ii < len(order); ii++ {
+				kk := order[ii]
+				_, ok := hasBeenUsed[kk]
+				if !ok {
+					firstAppearance += ", first appearance of " + intToStr(order[ii])
+					foundNew = true
+					hasBeenUsed[kk] = true
+				}
+			}
+			if foundNew {
+				if numFactors == maxSoFar {
+					fmt.Println("        ", number, "(", numFactors, "factors", firstAppearance, ")")
+				}
+			} else {
+				if numFactors == maxSoFar {
+					fmt.Println("        ", number, "(", numFactors, "factors )")
+				}
+			}
+		}
+		if numFactors == maxSoFar {
+		}
+		if numFactors > maxSoFar {
+			maxSoFar = numFactors
+			fmt.Println(number, "(", numFactors, "factors )")
+			showFactorization(primes, factors, order)
+		}
+	}
+}
+
+func factorNumber(primes []int, number int) {
+	fmt.Println("Factoring: ", number)
+	factors, order, _ := calcFactors(primes, number)
+	showFactorization(primes, factors, order)
+	numFactors := countFactors(primes, factors, order)
+	fmt.Println(numFactors, "factors")
+}
+
+func main() {
+	// uncomment one of these to run
+	// findMinWithFactors(36)
+	// findBillAlgoDifferent()
+	// findSeries(1000)
+	// findHighlyCompositeNumbers(10000)
+	// some simple factorization
+	primes := calcPrimes(100)
+	factorNumber(primes, 24)
+	factorNumber(primes, 60)
+	factorNumber(primes, 1440)
+	factorNumber(primes, 86400)
+}
